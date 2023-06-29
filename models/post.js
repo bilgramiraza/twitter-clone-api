3f99eafbd320aeccd2f9a9794d1b4da0e027e793
bilgramiraza <bilgramiraza@yahoo.com>
2023-06-30 00:35:37 +0530

    Defined defaults for properties

--- models/post.js
+++ models/post.js
@@ -5,8 +5,16 @@ const postSchema = new Schema({
   parentPost: { type:Schema.Types.ObjectId, ref:'post', default:null },
   post: { type:String, required:true },
   author: { type:Schema.Types.ObjectId, ref:'user', required:true },
-  likes: [{ type: Schema.Types.ObjectId, ref:'user' }],
-  comments:[{ type:Schema.Types.ObjectId, ref:'post' }],
+  likes: {
+    type:[{ type: Schema.Types.ObjectId, ref:'user' }],
+    default:function(){
+      return [this.author];
+    },
+  },
+  comments:{
+    type:[{ type:Schema.Types.ObjectId, ref:'post' }],
+    default:[],
+  },
 },{ timestamps: true });
 
 module.exports = mongoose.model('post', postSchema);
